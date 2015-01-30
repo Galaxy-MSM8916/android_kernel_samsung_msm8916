daff3e840e108746058446426811811b219f6110
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-01-30 11:52:37 +0530

    sched: Keep track of average nr_big_tasks
    Extend sched_get_nr_running_avg() API to return average nr_big_tasks,
    in addition to average nr_running and average nr_io_wait tasks. Also
    add a new trace point to record values returned by
    sched_get_nr_running_avg() API.
    
    Change-Id: Id3591e6d04da8db484b4d1cb9d95dba075f5ab9a
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4266,6 +4266,30 @@ static inline void unthrottle_offline_cfs_rqs(struct rq *rq) {}
 
 #endif /* CONFIG_CFS_BANDWIDTH */
 
+/*
+ * Return total number of tasks "eligible" to run on highest capacity cpu
+ *
+ * This is simply nr_big_tasks for cpus which are not of max_capacity and
+ * (nr_running - nr_small_tasks) for cpus of max_capacity
+ */
+unsigned int nr_eligible_big_tasks(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+	int nr_big = rq->hmp_stats.nr_big_tasks;
+	int nr = rq->nr_running;
+	int nr_small = rq->hmp_stats.nr_small_tasks;
+
+	if (rq->capacity != max_capacity)
+		return nr_big;
+
+	/* Consider all (except small) tasks on max_capacity cpu as big tasks */
+	nr_big = nr - nr_small;
+	if (nr_big < 0)
+		nr_big = 0;
+
+	return nr_big;
+}
+
 /**************************************************
  * CFS operations on tasks:
  */
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -746,6 +746,7 @@ extern void fixup_nr_big_small_task(int cpu);
 unsigned int max_task_load(void);
 extern void sched_account_irqtime(int cpu, struct task_struct *curr,
 				 u64 delta, u64 wallclock);
+extern unsigned int nr_eligible_big_tasks(int cpu);
 
 /*
  * 'load' is in reference to "best cpu" at its best frequency.
