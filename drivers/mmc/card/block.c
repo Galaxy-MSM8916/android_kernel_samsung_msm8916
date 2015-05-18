0b0d677b1680b4002bf1824d62965d548d5a7fc0
Ding Wang <justin.wang@spreadtrum.com>
2015-05-18 20:14:15 +0800

    mmc: card: Fixup request missing in mmc_blk_issue_rw_rq
    commit 29535f7b797df35cc9b6b3bca635591cdd3dd2a8 upstream.
    
    The current handler of MMC_BLK_CMD_ERR in mmc_blk_issue_rw_rq function
    may cause new coming request permanent missing when the ongoing
    request (previoulsy started) complete end.
    
    The problem scenario is as follows:
    (1) Request A is ongoing;
    (2) Request B arrived, and finally mmc_blk_issue_rw_rq() is called;
    (3) Request A encounters the MMC_BLK_CMD_ERR error;
    (4) In the error handling of MMC_BLK_CMD_ERR, suppose mmc_blk_cmd_err()
        end request A completed and return zero. Continue the error handling,
        suppose mmc_blk_reset() reset device success;
    (5) Continue the execution, while loop completed because variable ret
        is zero now;
    (6) Finally, mmc_blk_issue_rw_rq() return without processing request B.
    
    The process related to the missing request may wait that IO request
    complete forever, possibly crashing the application or hanging the system.
    
    Fix this issue by starting new request when reset success.
    
    Signed-off-by: Ding Wang <justin.wang@spreadtrum.com>
    Fixes: 67716327eec7 ("mmc: block: add eMMC hardware reset support")
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [@MSF-Jarvis: Modify this to wrap around existing error checks]
    
    Change-Id: I3dbee23dd15d95588a31e83dbbf46d261879f586

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2697,9 +2697,10 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 					 * h/w is in clean state and proceed
 					 * with new request.
 					 */
-					BUG_ON(card->host->areq);
-					goto start_new_req;
-				}
+					goto cmd_abort;
+				} else {
+                                        goto start_new_req;
+                                }
 				break;
 			}
 			goto cmd_abort;
