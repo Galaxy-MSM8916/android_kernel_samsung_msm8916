02d7ddf2ac702061476f705849612fc89dd04a84
Ravinder Konka <rkonka@codeaurora.org>
2015-08-14 20:42:41 +0530

    msm: ipa: fix to system suspend
    When there are no wakelocks acquired, system tries to go to
    suspend. And if any of IPA pipes are in polling state,
    system is not allowed go to suspend by IPA driver. As a result
    system resumes its operation from suspend to full state and this
    leads to bad power gains. Use a wakelock when any of the pipes
    are in polling state to not to make the system go to suspend in
    the first place. Release the wakelock when switching to interrupt mode.
    
    Change-Id: I0dbfe5c65dceeb487955b130788890dec71a7efd
    Acked-by: Chaitanya Pratapa <cpratapa@qti.qualcomm.com>
    Signed-off-by: Ravinder Konka <rkonka@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/791359
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/platform/msm/ipa/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_dp.c
@@ -231,6 +231,7 @@ static void ipa_handle_tx(struct ipa_sys_context *sys)
 	int cnt;
 
 	ipa_inc_client_enable_clks();
+	pm_stay_awake(ipa_ctx->pdev);
 	do {
 		cnt = ipa_handle_tx_core(sys, true, true);
 		if (cnt == 0) {
@@ -243,6 +244,7 @@ static void ipa_handle_tx(struct ipa_sys_context *sys)
 	} while (inactive_cycles <= POLLING_INACTIVITY_TX);
 
 	ipa_tx_switch_to_intr_mode(sys);
+	pm_relax(ipa_ctx->pdev);
 	ipa_dec_client_disable_clks();
 }
 
@@ -892,6 +894,7 @@ static void ipa_handle_rx(struct ipa_sys_context *sys)
 	int cnt;
 
 	ipa_inc_client_enable_clks();
+	pm_stay_awake(ipa_ctx->pdev);
 	do {
 		cnt = ipa_handle_rx_core(sys, true, true);
 		if (cnt == 0) {
@@ -904,6 +907,7 @@ static void ipa_handle_rx(struct ipa_sys_context *sys)
 	} while (inactive_cycles <= POLLING_INACTIVITY_RX);
 
 	ipa_rx_switch_to_intr_mode(sys);
+	pm_relax(ipa_ctx->pdev);
 	ipa_dec_client_disable_clks();
 }
 
