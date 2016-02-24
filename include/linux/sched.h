2eea1c3fde5825ff09ca3e3240f388470381e89a
Srinath Sridharan <srinathsr@google.com>
2016-02-24 14:41:14 -0800

    Introducing per-task/per-process cpufreq statistics.
    The statistics are provided in /proc/<pid>/cpufreq_stats as a read-only
    interface. This gives the amount of time spent by a task/process in
    each of the frequencies on each of the CPUs. The cat output will have
    "<cpu> <frequency> <time>" triplet in each line, which will mean that
    a given task spent <time> usertime units of time at <frequency> on
    CPU <cpu>. usertime units here is 10mS (similar to other time exported
    in /proc). The statistics are available during the lifetime of the
    tasks/processes.
    
    Change-Id: I3ebb338c27020aabd6b4e258bd26981c51a57771

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -52,6 +52,7 @@ struct sched_param {
 #include <linux/llist.h>
 #include <linux/uidgid.h>
 #include <linux/gfp.h>
+#include <linux/cpufreq.h>
 
 #include <asm/processor.h>
 
@@ -1200,6 +1201,27 @@ enum perf_event_task_context {
 	perf_nr_task_contexts,
 };
 
+
+#ifdef CONFIG_TASK_CPUFREQ_STATS
+struct task_cpufreq_stats {
+	int max_state;
+	/*
+	 * a table holding the current time
+	 * (in jiffies) on this CPU at
+	 * frequency freq_table[i]. freq_table can be
+         * obtained from drivers/cpufreq/freq_table.h.
+	 */
+	u64 *time_in_state;
+	/*
+	 * a table holding the cumulative time
+         * (in jiffies) spent by this task on this CPU
+	 * at frequency freq_table[i]. freq_table can be
+         * obtained from drivers/cpufreq/freq_table.h.
+	 */
+	u64 *cumulative_time_in_state;
+};
+#endif
+
 struct task_struct {
 	volatile long state;	/* -1 unrunnable, 0 runnable, >0 stopped */
 	void *stack;
@@ -1596,6 +1618,9 @@ struct task_struct {
 #if defined(CONFIG_BCACHE) || defined(CONFIG_BCACHE_MODULE)
 	unsigned int	sequential_io;
 	unsigned int	sequential_io_avg;
+#endif
+#ifdef CONFIG_TASK_CPUFREQ_STATS
+	struct task_cpufreq_stats cpufreq_stats[NR_CPUS];
 #endif
 };
 
