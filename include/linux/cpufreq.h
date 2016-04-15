0554b654a640db6ad0ffc52669b99bc91a513f71
jamison904 <james.jamison85@gmail.com>
2016-04-15 10:50:58 +0530

    cpufreq: Add BIOSHOCK governor
    Change-Id: I7823574db914b80c1c70b13e27283b8c508a89d7

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -442,6 +442,9 @@ extern struct cpufreq_governor cpufreq_gov_relaxed;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
