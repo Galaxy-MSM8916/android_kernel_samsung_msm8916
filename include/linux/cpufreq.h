2c271221feba50c9b1d78fb687f78022ae8603bf
sultanqasim <sultanqasim@gmail.com>
2016-04-16 19:56:03 +0530

    cpufreq: Add LIONFISH governor
    Change-Id: I3ceeb7ced0dabbaecf68e3d9f20c2edf9c502295

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -445,6 +445,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
