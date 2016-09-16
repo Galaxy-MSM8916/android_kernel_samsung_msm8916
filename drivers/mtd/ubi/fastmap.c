3df055460b0865c3ab8487f1af056c49f1248592
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-09-16 16:59:12 +0200

    UBI: fastmap: scrub PEB when bitflips are detected in a free PEB EC header
    commit ecbfa8eabae9cd73522d1d3d15869703c263d859 upstream.
    
    scan_pool() does not mark the PEB for scrubing when bitflips are
    detected in the EC header of a free PEB (VID header region left to
    0xff).
    Make sure we scrub the PEB in this case.
    
    Change-Id: I75cd64c176e7d9dcc559cc41f5175a29dae6bb66
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Fixes: dbb7d2a88d2a ("UBI: Add fastmap core")
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/mtd/ubi/fastmap.c
+++ drivers/mtd/ubi/fastmap.c
@@ -463,12 +463,16 @@ static int scan_pool(struct ubi_device *ubi, struct ubi_attach_info *ai,
 					be64_to_cpu(ech->last_erase_time);
 			unmap_peb(ai, pnum);
 			dbg_bld("Adding PEB to free: %i", pnum);
+
 			if (err == UBI_IO_FF_BITFLIPS)
 				add_aeb(ai, free, pnum, ec, last_erase_time,
 						0, 1);
 			else
 				add_aeb(ai, free, pnum, ec, last_erase_time,
 						0, 0);
+				scrub = 1;
+
+                        add_aeb(ai, free, pnum, ec, scrub);
 			continue;
 		} else if (err == 0 || err == UBI_IO_BITFLIPS) {
 			dbg_bld("Found non empty PEB:%i in pool", pnum);
