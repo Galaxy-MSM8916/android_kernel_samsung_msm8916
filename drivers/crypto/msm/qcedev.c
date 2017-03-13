86379ea327e3bcebbb48c5b370533585c04feb26
AnilKumar Chimata <anilc@codeaurora.org>
2017-03-13 16:13:47 +0530

    crypto: msm: Fix buffer overflow issue
    In multi-threaded environment diglen variable could be modified
    by multiple threads at the same time. Buffer overflow might
    happen in current thread if another thread changes the diglen
    variable. So add mutex locks to avoid this issue.
    
    Change-Id: I62c63c55c028dedb1dd0eec862851bd8e818a5d3
    Signed-off-by: AnilKumar Chimata <anilc@codeaurora.org>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1763,6 +1763,7 @@ long qcedev_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 			mutex_unlock(&hash_access_lock);
 			return -EINVAL;
 		}
+
 		qcedev_areq.sha_op_req.diglen = handle->sha_ctxt.diglen;
 		memcpy(&qcedev_areq.sha_op_req.digest[0],
 				&handle->sha_ctxt.digest[0],
@@ -1806,6 +1807,7 @@ long qcedev_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 			mutex_unlock(&hash_access_lock);
 			return -EINVAL;
 		}
+
 		qcedev_areq.sha_op_req.diglen =	handle->sha_ctxt.diglen;
 		memcpy(&qcedev_areq.sha_op_req.digest[0],
 				&handle->sha_ctxt.digest[0],
