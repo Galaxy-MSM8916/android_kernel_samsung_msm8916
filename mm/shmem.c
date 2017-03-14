6a062f1c5a00e10fd380a53017428b35300fcb8f
Angelo G. Del Regno <kholk11@gmail.com>
2017-03-15 00:56:03 +0100

    mm: shmem: Reschedule by unlocking and relocking RCU because of missing API
    The commit introducing the call to cond_resched_rcu() is backported
    from a recent kernel version, which has got some very good updates
    to the RCU, including a new function cond_resched_rcu which is
    doing not-so-complicated rescheduling stuff.
    Kernel 3.10 hasn't got any of these and porting would be overkill.
    
    On our current code base, the RCU management is pretty stupid
    compared to newer kernels, so it's just ok to reschedule by just
    unlocking the RCU and relocking it: this will allow to update its
    status and the drivers will be happy.

--- mm/shmem.c
+++ mm/shmem.c
@@ -1936,7 +1936,8 @@ restart:
 		}
 
 		if (need_resched()) {
-			cond_resched_rcu();
+			rcu_read_unlock();
+			rcu_read_lock();
 			start = iter.index + 1;
 			goto restart;
 		}
@@ -2006,7 +2007,8 @@ restart:
 			spin_unlock_irq(&mapping->tree_lock);
 continue_resched:
 			if (need_resched()) {
-				cond_resched_rcu();
+				rcu_read_unlock();
+				rcu_read_lock();
 				start = iter.index + 1;
 				goto restart;
 			}
