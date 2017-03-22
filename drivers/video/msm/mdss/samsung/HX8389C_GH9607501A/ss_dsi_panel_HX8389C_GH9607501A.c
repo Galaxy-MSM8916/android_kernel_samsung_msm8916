87e81a77d5f9618eefdac3a8c3e363688c44d760
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-21 20:43:17 -0400

    mdss: Work around HX8389C bootloader mdss string change
    * Hardcoded mdss string in the bootloader command line passed to the
      kernel causes boot to fail on kernels with screen drivers not
      programmed to accept the old/new string. Fix this by checking for the
      presence of either of both strings at boot and initialising the
      display if at least one of them matches.
    
    * Overwrite the old mdss string with the new one if a display with
      the old naming is found.
    
    * Print the the new panel name for the HX8389C display

--- drivers/video/msm/mdss/samsung/HX8389C_GH9607501A/ss_dsi_panel_HX8389C_GH9607501A.c
+++ drivers/video/msm/mdss/samsung/HX8389C_GH9607501A/ss_dsi_panel_HX8389C_GH9607501A.c
@@ -380,12 +380,19 @@ static void mdss_panel_init(struct samsung_display_driver_data *vdd)
 static int __init samsung_panel_init(void)
 {
 	struct samsung_display_driver_data *vdd = samsung_get_vdd();
-	char panel_string[] = "ss_dsi_panel_HX8389C_GH9607501A_QHD";
+	char old_panel_string[] = "qcom,mdss_dsi_hx8389c_qhd_video";
+	char new_panel_string[] = "ss_dsi_panel_HX8389C_GH9607501A_QHD";
 
 	vdd->panel_name = mdss_mdp_panel + 8;
 	pr_info("%s : %s\n", __func__, vdd->panel_name);
 
-	if (!strncmp(vdd->panel_name, panel_string, strlen(panel_string)))
+	if (!strncmp(vdd->panel_name, old_panel_string, strlen(old_panel_string))) {
+		strncpy(vdd->panel_name, new_panel_string, strlen(new_panel_string)+1);
+		strncat(vdd->panel_name, ":1:none", 7);
+		pr_info("%s : new panel name: %s\n", __func__, vdd->panel_name);
+		vdd->panel_func.samsung_panel_init = mdss_panel_init;
+	}
+	else if (!strncmp(vdd->panel_name, new_panel_string, strlen(new_panel_string)))
 		vdd->panel_func.samsung_panel_init = mdss_panel_init;
 
 	return 0;
