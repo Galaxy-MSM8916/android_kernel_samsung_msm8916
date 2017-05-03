6f9c5becb57a6dd4020534bb2d0f29603fa82d8f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-03 13:11:01 -0400

    sensors: Update GP2A002 sensor driver

--- drivers/sensors/gp2a002.c
+++ drivers/sensors/gp2a002.c
@@ -47,21 +47,21 @@
 #define REGS_OPMOD		0x4 /* Write Only */
 #define REGS_CON		0x6 /* Write Only */
 
-#if defined(CONFIG_SENSORS_GP2A_MODE_A)
+#if defined(CONFIG_SENSORS_GP2A_MODE_A) || defined(CONFIG_SENSORS_BMA2X2_MODE_A)
 #define PROX_NONDETECT		0xC2
 #define PROX_DETECT		0xC2
 #define PROX_NONDETECT_MODE1	0xC8
 #define PROX_DETECT_MODE1	0xC8
 #define PROX_NONDETECT_MODE2	0xCB
 #define PROX_DETECT_MODE2	0xCB
-#elif defined(CONFIG_SENSORS_GP2A_MODE_B1)
+#elif defined(CONFIG_SENSORS_GP2A_MODE_B1) || defined(CONFIG_SENSORS_BMA2X2_MODE_B1)
 #define PROX_NONDETECT		0x40
 #define PROX_DETECT		0x20
 #define PROX_NONDETECT_MODE1	0x43
 #define PROX_DETECT_MODE1	0x28
 #define PROX_NONDETECT_MODE2	0x48
 #define PROX_DETECT_MODE2	0x42
-#elif defined(CONFIG_SENSORS_GP2A_MODE_B15)
+#elif defined(CONFIG_SENSORS_GP2A_MODE_B15) || defined(CONFIG_SENSORS_PROX_NEWMODE2)
 #define PROX_NONDETECT		0x2F
 #define PROX_DETECT		0x0D
 #define PROX_NONDETECT_MODE1	0x43
@@ -79,7 +79,7 @@
 #define OFFSET_FILE_PATH		"/efs/FactoryApp/prox_cal"
 
 #define PROXIMITY	1
-#define CHIP_DEV_NAME	"GP2A002"
+#define CHIP_DEV_NAME	"GP2AP002"
 #define CHIP_DEV_VENDOR	"SHARP"
 
 struct gp2a_data;
@@ -106,6 +106,7 @@ struct gp2a_data {
 
 	u8 detect;
 	u8 nondetect;
+	struct regulator *leda_3p0;
 };
 
 int gp2a_i2c_read(struct gp2a_data *gp2a, u8 reg, u8 *val)
@@ -175,6 +176,7 @@ int gp2a_i2c_write(struct gp2a_data *gp2a, u8 reg, u8 val)
 
 	return err;
 }
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 static int gp2a_leda_onoff(struct gp2a_data *gp2a, int power)
 {
 	int ret;
@@ -188,13 +190,16 @@ static int gp2a_leda_onoff(struct gp2a_data *gp2a, int power)
 
 	return 0;
 }
+#endif
 static int gp2a_power_onoff(struct gp2a_data *gp2a, int power)
 {
 	u8 value;
 	pr_info("%s,status(%d)\n", __func__, power);
 
 	if (power) {
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 		gp2a_leda_onoff(gp2a, power);
+#endif
 		value = 0x18;
 		gp2a_i2c_write(gp2a, REGS_CON, value);
 		value = 0x08;
@@ -217,7 +222,9 @@ static int gp2a_power_onoff(struct gp2a_data *gp2a, int power)
 
 		value = 0x02;
 		gp2a_i2c_write(gp2a, REGS_OPMOD, value);
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 		gp2a_leda_onoff(gp2a, power);
+#endif
 	}
 	return 0;
 }
@@ -259,7 +266,7 @@ static int gp2a_cal_mode_read_file(struct gp2a_data *gp2a)
 	old_fs = get_fs();
 	set_fs(KERNEL_DS);
 
-	cal_mode_filp = filp_open(OFFSET_FILE_PATH, O_RDONLY, 0);
+	cal_mode_filp = filp_open(OFFSET_FILE_PATH, O_RDONLY, 0666);
 	if (IS_ERR(cal_mode_filp)) {
 		err = PTR_ERR(cal_mode_filp);
 		if (err != -ENOENT)
@@ -295,7 +302,7 @@ static int gp2a_cal_mode_save_file(char mode)
 	set_fs(KERNEL_DS);
 
 	cal_mode_filp = filp_open(OFFSET_FILE_PATH,
-		O_CREAT | O_TRUNC | O_WRONLY, 0660);
+		O_CREAT | O_TRUNC | O_WRONLY, 0666);
 	if (IS_ERR(cal_mode_filp)) {
 		pr_err("%s,Can't open cal_mode file\n",
 			__func__);
@@ -379,6 +386,41 @@ static struct device_attribute *proxi_attrs[] = {
 	&dev_attr_prox_cal,
 	NULL,
 };
+
+#ifdef CONFIG_SENSORS_GP2A002_PMIC_LEDA
+static void gp2a_pmic_leda_onoff(struct gp2a_data *info, int onoff)
+{
+	int ret;
+
+	pr_err("%s %s\n", __func__, (onoff) ? "on" : "off");
+
+	if (info->leda_3p0 == NULL) {
+		info->leda_3p0 = regulator_get(&info->i2c_client->dev,
+			"PROX_LEDA_3.0V");
+		if (IS_ERR(info->leda_3p0)) {
+			pr_err("%s: regulator_get failed for PROX_LEDA_3.0V\n",
+				__func__);
+				return;
+		}
+	}
+
+	if (onoff) {
+		ret = regulator_enable(info->leda_3p0);
+		if (ret)
+			pr_err("%s: leda_3p3 enable failed (%d)\n",
+				__func__, ret);
+	} else {
+		ret = regulator_disable(info->leda_3p0);
+		if (ret)
+			pr_err("%s: leda_3p0 disable failed (%d)\n",
+				__func__, ret);
+	}
+	msleep(20);
+	return;
+}
+
+#endif
+
 static int gp2a_regulator_onoff(struct device *dev, bool onoff)
 {
 	struct regulator *gp2a_vio;
@@ -485,6 +527,7 @@ static ssize_t proximity_enable_store(struct device *dev,
 				gp2a->nondetect = PROX_NONDETECT;
 				gp2a->detect = PROX_DETECT;
 			}
+			gp2a_regulator_onoff(&gp2a->i2c_client->dev, true);
 			gp2a_power_onoff(gp2a, 1);
 			gp2a->power_state = value;
 
@@ -494,6 +537,7 @@ static ssize_t proximity_enable_store(struct device *dev,
 			input_sync(gp2a->input);
 		} else {
 			gp2a_power_onoff(gp2a, 0);
+			gp2a_regulator_onoff(&gp2a->i2c_client->dev, false);
 			gp2a->power_state = value;
 		}
 
@@ -610,6 +654,8 @@ err_gpio_direction_input:
 done:
 	return rc;
 }
+
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 static int gp2a_request_gpio(struct gp2a_platform_data *pdata)
 {
 	int ret;
@@ -627,8 +673,7 @@ static int gp2a_request_gpio(struct gp2a_platform_data *pdata)
 	}
 	return 0;
 }
-
-
+#endif
 static int gp2a_parse_dt(struct device *dev, struct gp2a_platform_data *pdata)
 {
 	struct device_node *np = dev->of_node;
@@ -640,6 +685,7 @@ static int gp2a_parse_dt(struct device *dev, struct gp2a_platform_data *pdata)
 		pr_err("%s : get irq_gpio(%d) error\n", __func__, pdata->p_out);
 		return -ENODEV;
 	}
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 	pdata->power_en = of_get_named_gpio_flags(np, "gp2a-i2c,en-gpio",
 				0, &flags);
 	if (pdata->power_en < 0) {
@@ -647,6 +693,7 @@ static int gp2a_parse_dt(struct device *dev, struct gp2a_platform_data *pdata)
 			pdata->power_en);
 		return -ENODEV;
 	}
+#endif
 	return 0;
 }
 
@@ -677,9 +724,11 @@ static int gp2a_i2c_probe(struct i2c_client *client,
 		ret = gp2a_parse_dt(&client->dev, pdata);
 		if (ret < 0)
 			return ret;
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 		ret = gp2a_request_gpio(pdata);
 		if (ret < 0)
 			return ret;
+#endif
 	}
 
 	if (!pdata) {
@@ -740,7 +789,11 @@ static int gp2a_i2c_probe(struct i2c_client *client,
 
 	INIT_WORK(&gp2a->work_prox, gp2a_prox_work_func);
 
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 	gp2a_leda_onoff(gp2a, 1);
+#else
+	gp2a_pmic_leda_onoff(gp2a, 1);
+#endif
 
 	ret = gp2a_setup_irq(gp2a);
 
@@ -749,7 +802,9 @@ static int gp2a_i2c_probe(struct i2c_client *client,
 		goto err_setup_irq;
 	}
 
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 	gp2a_leda_onoff(gp2a, 0);
+#endif
 
 	ret = sensors_register(gp2a->dev, gp2a,
 		proxi_attrs, "proximity_sensor");
@@ -766,7 +821,9 @@ exit_gp2a_sensors_register:
 	free_irq(gp2a->irq, gp2a);
 	gpio_free(gp2a->pdata->p_out);
 err_setup_irq:
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
 	gp2a_leda_onoff(gp2a, 0);
+#endif
 	sysfs_remove_group(&gp2a->input->dev.kobj, &proximity_attribute_group);
 err_sysfs_create_group_proximity:
 	sensors_remove_symlink(&gp2a->input->dev.kobj, gp2a->input->name);
@@ -779,7 +836,32 @@ err_input_allocate_device_proximity:
 	wake_lock_destroy(&gp2a->prx_wake_lock);
 	kfree(gp2a);
 done:
+	gp2a_regulator_onoff(&client->dev, false);
 	return ret;
+}
+
+static void gp2a_i2c_shutdown(struct i2c_client *client)
+{
+	struct gp2a_data *gp2a = i2c_get_clientdata(client);
+	if (!gp2a)
+		return;
+	if (gp2a->power_state) {
+		disable_irq_wake(gp2a->irq);
+		disable_irq(gp2a->irq);
+		msleep(20);
+	}
+
+	free_irq(gp2a->irq, gp2a);
+	gpio_free(gp2a->pdata->p_out);
+#ifndef CONFIG_SENSORS_GP2A002_PMIC_LEDA
+	gp2a_leda_onoff(gp2a, 0);
+#endif
+	sysfs_remove_group(&gp2a->input->dev.kobj, &proximity_attribute_group);
+	sensors_remove_symlink(&gp2a->input->dev.kobj, gp2a->input->name);
+	input_unregister_device(gp2a->input);
+	mutex_destroy(&gp2a->power_lock);
+	wake_lock_destroy(&gp2a->prx_wake_lock);
+	kfree(gp2a);
 }
 
 static const struct i2c_device_id gp2a_device_id[] = {
@@ -803,6 +885,7 @@ static struct i2c_driver gp2a_i2c_driver = {
 
 	},
 	.probe		= gp2a_i2c_probe,
+	.shutdown	= gp2a_i2c_shutdown,
 	.id_table	= gp2a_device_id,
 };
 
