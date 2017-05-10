011d83cbb4dc062c623a4d60eaa2a847387d121d
deadman96385 <seanhoyt963@gmail.com>
2017-05-10 03:04:35 -0500

    Revert "camera_v2: Rename header files to allow camera code coexistence"
    This reverts commit b5b29086bff59f767071868b20ba01dd0a197043.

--- include/media/msm_camsensor_sdk.h
+++ include/media/msm_camsensor_sdk.h
@@ -63,20 +63,6 @@ enum msm_sensor_power_seq_type_t {
 	SENSOR_I2C,
 };
 
-enum msm_camera_qup_i2c_write_batch_size_t {
-	MSM_CAMERA_I2C_BATCH_SIZE_1 = 1,
-	MSM_CAMERA_I2C_BATCH_SIZE_2,
-	MSM_CAMERA_I2C_BATCH_SIZE_3,
-	MSM_CAMERA_I2C_BATCH_SIZE_4,
-	MSM_CAMERA_I2C_BATCH_SIZE_5,
-	MSM_CAMERA_I2C_BATCH_SIZE_MAX,
-};
-
-enum msm_camera_qup_i2c_write_batch_t {
-	MSM_CAMREA_I2C_BATCH_DISABLE = 0,
-	MSM_CAMERA_I2C_BATCH_ENABLE,
-};
-
 enum msm_camera_i2c_reg_addr_type {
 	MSM_CAMERA_I2C_BYTE_ADDR = 1,
 	MSM_CAMERA_I2C_WORD_ADDR,
@@ -180,12 +166,6 @@ enum msm_flash_cfg_type_t {
 	CFG_FLASH_HIGH,
 };
 
-enum msm_sensor_output_format_t {
-	MSM_SENSOR_BAYER,
-	MSM_SENSOR_YCBCR,
-	MSM_SENSOR_META,
-};
-
 struct msm_sensor_power_setting {
 	enum msm_sensor_power_seq_type_t seq_type;
 	uint16_t seq_val;
@@ -232,7 +212,6 @@ struct msm_camera_sensor_slave_info {
 	uint8_t  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	uint8_t is_flash_supported;
-	enum msm_sensor_output_format_t output_format;
 };
 
 struct msm_camera_i2c_reg_array {
@@ -247,7 +226,6 @@ struct msm_camera_i2c_reg_setting {
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	enum msm_camera_i2c_data_type data_type;
 	uint16_t delay;
-	enum msm_camera_qup_i2c_write_batch_t qup_i2c_batch;
 };
 
 struct msm_camera_csid_vc_cfg {
@@ -312,8 +290,6 @@ struct region_params_t {
 	*/
 	uint16_t step_bound[2];
 	uint16_t code_per_step;
-	/* qvalue for converting float type numbers to integer format */
-	uint32_t qvalue;
 };
 
 struct reg_settings_t {
