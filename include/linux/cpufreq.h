91e82c4f3df251ac0feee76834e7e11823b06a57
GeneticEnginer <igor_cestari@hotmail.com>
2017-06-02 17:10:09 -0300

    enable ondemand/conservative cpu gov and add lionfish cpu gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -432,6 +432,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
