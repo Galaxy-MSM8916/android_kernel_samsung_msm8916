75f6e0da978c1cb4a4ae62f50c5d5b486aa118a4
GeneticEnginer <igor_cestari@hotmail.com>
2017-06-11 00:05:08 -0300

    disable cpusets due to slowdowns; add msm hotplug; turn intelliplug off by default; bump kernel version

--- arch/arm/hotplug/intelliplug/intelli_plug.c
+++ arch/arm/hotplug/intelliplug/intelli_plug.c
@@ -52,7 +52,7 @@ static struct delayed_work intelli_plug_boost;
 static struct workqueue_struct *intelliplug_wq;
 static struct workqueue_struct *intelliplug_boost_wq;
 
-static unsigned int intelli_plug_active = 1;
+static unsigned int intelli_plug_active = 0;
 module_param(intelli_plug_active, uint, 0644);
 
 static unsigned int touch_boost_active = 1;
