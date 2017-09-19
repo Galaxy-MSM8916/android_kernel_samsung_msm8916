9bae08d4d3de3cd8bb45db011f84fe390fe67635
raghavendra ambadas <rambad@codeaurora.org>
2017-09-19 17:30:22 +0530

    msm: mdss: update the wait logic for sending DSI commands
    For video mode panels, the assumption is that the vertical
    blanking region will be maximum of 4 ms and hence, we wait for
    4 ms before queuing the DSI DMA commands. With the incell panels
    having huge vertical blanking region and dynamic fps feature,
    this assumption is no longer safe. Update the wait logic by
    calculating the vertical blanking duration.
    
    Change-Id: I69fdc182342493a54d78ae3ce5f4729e17452155
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -32,6 +32,8 @@
 #define DMA_TX_TIMEOUT 200
 #define DMA_TPG_FIFO_LEN 64
 
+#define CEIL(x, y)		(((x) + ((y) - 1)) / (y))
+
 struct mdss_dsi_ctrl_pdata *ctrl_list[DSI_CTRL_MAX];
 
 struct mdss_hw mdss_dsi0_hw = {
@@ -1978,14 +1980,22 @@ void mdss_dsi_wait4video_done(struct mdss_dsi_ctrl_pdata *ctrl)
 static int mdss_dsi_wait4video_eng_busy(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	int ret = 0;
+	u32 v_total = 0, v_blank = 0, sleep_ms = 0, fps = 0;
+	struct mdss_panel_info *pinfo = &ctrl->panel_data.panel_info;
 
 	if (ctrl->panel_mode == DSI_CMD_MODE)
 		return ret;
 
 	if (ctrl->ctrl_state & CTRL_STATE_MDP_ACTIVE) {
 		mdss_dsi_wait4video_done(ctrl);
-		/* delay 4 ms to skip BLLP */
-		usleep(4000);
+		v_total = mdss_panel_get_vtotal(pinfo);
+		v_blank = pinfo->lcdc.v_back_porch + pinfo->lcdc.v_pulse_width;
+		fps = pinfo->mipi.frame_rate;
+
+		sleep_ms = CEIL((v_blank * 1000), (v_total * fps));
+		/* delay sleep_ms to skip BLLP */
+		if (sleep_ms)
+			usleep(sleep_ms * 1000);
 		ret = 1;
 	}
 
