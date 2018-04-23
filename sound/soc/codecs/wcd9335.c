ba8bb6109f50df07f5bdda5d2e3713c8da05cd23
Dyneteve <sound0020@gmail.com>
2018-04-23 13:47:35 -0400

    config: Disable SAMSUNG_AUDIO
    Change-Id: Ibebfeed9d3b13e95d9b56ec89a7c15fbb9d4f352

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -4128,18 +4128,14 @@ static int tasha_codec_enable_swr(struct snd_soc_dapm_widget *w,
 {
 	struct snd_soc_codec *codec = w->codec;
 	struct tasha_priv *tasha;
-#ifndef CONFIG_SAMSUNG_AUDIO
 	int i;
-#endif
 
 	tasha = snd_soc_codec_get_drvdata(codec);
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
-#ifndef CONFIG_SAMSUNG_AUDIO
 		for (i = 0; i < tasha->nr; i++)
 			swrm_wcd_notify(tasha->swr_ctrl_data[i].swr_pdev,
 					SWR_DEVICE_UP, NULL);
-#endif
 		break;
 	}
 	return 0;
@@ -11451,18 +11447,14 @@ static int tasha_device_down(struct wcd9xxx *wcd9xxx)
 	struct snd_soc_codec *codec;
 	struct tasha_priv *priv;
 	int count;
-#ifndef CONFIG_SAMSUNG_AUDIO
 	int i = 0;
-#endif
 
 	codec = (struct snd_soc_codec *)(wcd9xxx->ssr_priv);
 	priv = snd_soc_codec_get_drvdata(codec);
 	wcd_cpe_ssr_event(priv->cpe_core, WCD_CPE_BUS_DOWN_EVENT);
-#ifndef CONFIG_SAMSUNG_AUDIO
 	for (i = 0; i < priv->nr; i++)
 		swrm_wcd_notify(priv->swr_ctrl_data[i].swr_pdev,
 				SWR_DEVICE_DOWN, NULL);
-#endif
 	snd_soc_card_change_online_state(codec->card, 0);
 	for (count = 0; count < NUM_CODEC_DAIS; count++)
 		priv->dai[count].bus_down_in_recovery = true;
