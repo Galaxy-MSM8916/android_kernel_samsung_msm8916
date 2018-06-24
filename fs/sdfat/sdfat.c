a85a2789ae15059afd04ba3cbfed81fe8a50dba9
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-06-24 19:09:59 -0400

    Revert "fs: sdfat: Add MODULE_ALIAS_FS for supported filesystems"
    This reverts commit 0e27f22d365a8889070a1feb2032c01edcfdf191.

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -4997,7 +4997,6 @@ static struct file_system_type sdfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
-MODULE_ALIAS_FS("sdfat");
 
 #ifdef CONFIG_SDFAT_USE_FOR_EXFAT
 static struct file_system_type exfat_fs_type = {
@@ -5011,7 +5010,6 @@ static struct file_system_type exfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
-MODULE_ALIAS_FS("exfat");
 #endif /* CONFIG_SDFAT_USE_FOR_EXFAT */
 
 #ifdef CONFIG_SDFAT_USE_FOR_VFAT
@@ -5026,7 +5024,6 @@ static struct file_system_type vfat_fs_type = {
 #endif /* CONFIG_SDFAT_DBG_IOCTL */
 	.fs_flags    = FS_REQUIRES_DEV,
 };
-MODULE_ALIAS_FS("vfat");
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
 
 static int __init init_sdfat_fs(void)
