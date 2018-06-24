8195a791fdd0479d1019358dcb4f032d1ca31a71
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-06-24 19:10:00 -0400

    Revert "fs: sdfat: Update to version 2.0.6-lineage"
    This reverts commit f32030da468dcf1e002e539479be0ba6a4601f1e.

--- fs/sdfat/fatent.c
+++ fs/sdfat/fatent.c
@@ -50,8 +50,7 @@
  */
 static s32 exfat_ent_get(struct super_block *sb, u32 loc, u32 *content)
 {
-	u32 off, _content;
-	u64 sec;
+	u32 sec, off, _content;
 	u8 *fat_sector;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
 
@@ -75,8 +74,7 @@ static s32 exfat_ent_get(struct super_block *sb, u32 loc, u32 *content)
 
 static s32 exfat_ent_set(struct super_block *sb, u32 loc, u32 content)
 {
-	u32 off;
-	u64 sec;
+	u32 sec, off;
 	u8 *fat_sector;
 	__le32 *fat_entry;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
@@ -98,8 +96,7 @@ static s32 exfat_ent_set(struct super_block *sb, u32 loc, u32 content)
 #define FATENT_FAT32_IGNORE_MASK	(0xF0000000U)
 static s32 fat32_ent_get(struct super_block *sb, u32 loc, u32 *content)
 {
-	u32 off, _content;
-	u64 sec;
+	u32 sec, off, _content;
 	u8 *fat_sector;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
 
@@ -125,8 +122,7 @@ static s32 fat32_ent_get(struct super_block *sb, u32 loc, u32 *content)
 
 static s32 fat32_ent_set(struct super_block *sb, u32 loc, u32 content)
 {
-	u32 off;
-	u64 sec;
+	u32 sec, off;
 	u8 *fat_sector;
 	__le32 *fat_entry;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
@@ -150,8 +146,7 @@ static s32 fat32_ent_set(struct super_block *sb, u32 loc, u32 content)
 #define FATENT_FAT16_VALID_MASK		(0x0000FFFFU)
 static s32 fat16_ent_get(struct super_block *sb, u32 loc, u32 *content)
 {
-	u32 off, _content;
-	u64 sec;
+	u32 sec, off, _content;
 	u8 *fat_sector;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
 
@@ -177,8 +172,7 @@ static s32 fat16_ent_get(struct super_block *sb, u32 loc, u32 *content)
 
 static s32 fat16_ent_set(struct super_block *sb, u32 loc, u32 content)
 {
-	u32 off;
-	u64 sec;
+	u32 sec, off;
 	u8 *fat_sector;
 	__le16 *fat_entry;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
@@ -201,8 +195,7 @@ static s32 fat16_ent_set(struct super_block *sb, u32 loc, u32 content)
 #define FATENT_FAT12_VALID_MASK		(0x00000FFFU)
 static s32 fat12_ent_get(struct super_block *sb, u32 loc, u32 *content)
 {
-	u32 off, _content;
-	u64 sec;
+	u32 sec, off, _content;
 	u8 *fat_sector;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
 
@@ -242,8 +235,7 @@ static s32 fat12_ent_get(struct super_block *sb, u32 loc, u32 *content)
 
 static s32 fat12_ent_set(struct super_block *sb, u32 loc, u32 content)
 {
-	u32 off;
-	u64 sec;
+	u32 sec, off;
 	u8 *fat_sector, *fat_entry;
 	FS_INFO_T *fsi = &(SDFAT_SB(sb)->fsi);
 
