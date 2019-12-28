4264af0c4969affe7728034a455ab28dbfaad3c0
HacKurx <hackurx@gmail.com>
2019-12-28 13:40:01 +0100

    slub: add check for write-after-free
    From: Daniel Micay <danielmicay@gmail.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2402,6 +2402,11 @@ redo:
 		stat(s, ALLOC_FASTPATH);
 	}
 
+	if (!(s->flags & (SLAB_DESTROY_BY_RCU | SLAB_POISON)) && !s->ctor && object) {
+		size_t offset = s->offset ? 0 : sizeof(void *);
+		BUG_ON(memchr_inv((void *)object + offset, 0, s->object_size - offset));
+	}
+
 	if (unlikely(gfpflags & __GFP_ZERO) && object)
 		memset(object, 0, s->object_size);
 
