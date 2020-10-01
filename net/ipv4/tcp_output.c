03c24c550dd9e2705931bc8e9b13f5fc086bfbab
Zeeshan Ahmad <itexpert120@gmail.com>
2020-10-01 10:41:33 +0500

    network speed tweak

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -247,7 +247,10 @@ void tcp_select_initial_window(int __space, __u32 mss,
 		else
 			*rcv_wnd = min(*rcv_wnd, init_cwnd * mss);
 	}
-
+	
+	/* Set and Lock the Initial TCP window size to 64K*/
+        *rcv_wnd = 64240;
+	
 	/* Set the clamp no higher than max representable value */
 	(*window_clamp) = min(65535U << (*rcv_wscale), *window_clamp);
 }
